efe434b00faa355c37b7b1c471a4914e9ea6cea5
Ifrim Silviu <silviu.redent@gmail.com>
2022-07-01 20:16:04 +0300

    Complete seed function

--- flats-app/prisma/seed.ts
+++ flats-app/prisma/seed.ts
@@ -2,23 +2,16 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-    await prisma.flat.upsert({
-        where: {},
-        update: {},
-        create: {
+    [
+        {
             title: 'The best apartment in Prague',
             image_url: 'https://img.staticmb.com/mbcontent//images/uploads/2021/7/flat-vs-independent-house.jpg',
         },
-    });
-
-    await prisma.flat.upsert({
-        where: {},
-        update: {},
-        create: {
+        {
             title: 'Best flats in Plsen!',
             image_url: 'https://img.staticmb.com/mbcontent//images/uploads/2021/7/flat-vs-independent-house.jpg'
-        },
-    });
+        }
+    ].forEach(async flat => await prisma.flat.create({ data: flat }));
 }
 
 main()

c1ad4622c952dac2ca5f4e070353fbefa189f04f
Ifrim Silviu <silviu.redent@gmail.com>
2022-07-02 19:54:35 +0300

    Add base layout.

--- flats-app/app/root.tsx
+++ flats-app/app/root.tsx
@@ -1,16 +1,65 @@
-import { LiveReload, Outlet } from "@remix-run/react";
+import * as React from 'react';
+import type { LinksFunction } from '@remix-run/node';
+import {
+  Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration, useCatch, useLocation,
+} from '@remix-run/react';
+import { Box } from '@mui/material';
+import ThemeConfig from '~/themes';
+/**
+ * The `links` export is a function that returns an array of objects that map to
+ * the attributes for an HTML `<link>` element. These will load `<link>` tags on
+ * every route in the app, but individual routes can include their own links
+ * that are automatically unloaded when a user navigates away from the route.
+ *
+ * https://remix.run/api/app#links
+ */
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' },
+];
 
-export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <title>Remix: So great, it's funny!</title>
-      </head>
-      <body>
+type DocumentProps = { children: React.ReactNode; title?: string } & typeof defaultProps;
+
+const defaultProps = { title: 'Remix Run' };
+const Document = ({ children, title }: DocumentProps) => (
+  <html lang="en">
+    <head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
+
+      {title ? <title>{title}</title> : null}
+      <Meta />
+      <Links />
+    </head>
+    <body>
+      {children}
+      <ScrollRestoration />
+      <Scripts />
+      {process.env.NODE_ENV === 'development' && <LiveReload />}
+    </body>
+  </html>
+);
+
+Document.defaultProps = defaultProps;
+
+const Layout = ({ children }: React.PropsWithChildren<{}>) => (
+  <Box display="flex" minHeight="100vh">
+    {children}
+  </Box>
+);
+
+/**
+ * The root module's default export is a component that renders the current
+ * route via the `<Outlet />` component. Think of this as the global layout
+ * component for your app.
+ */
+const App = () => (
+  <Document>
+    <ThemeConfig>
+      <Layout>
         <Outlet />
-        <LiveReload />
-      </body>
-    </html>
-  );
-}
+      </Layout>
+    </ThemeConfig>
+  </Document>
+);
+
+export default App;

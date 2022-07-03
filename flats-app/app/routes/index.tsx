556750deba2366f302924990f70534e9a53ceddf
Ifrim Silviu <silviu.redent@gmail.com>
2022-07-03 13:05:43 +0300

    Add flat card component.

--- flats-app/app/routes/index.tsx
+++ flats-app/app/routes/index.tsx
@@ -1,32 +1,31 @@
-import { Box, Chip, Stack, Typography } from "@mui/material";
+import { Stack } from "@mui/material";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
-import React from "react";
 import { getFlats } from "~/models/flat.server";
+import FlatCard from "~/src/components/FlatCard";
 
 type LoaderData = {
-    flats: Awaited<ReturnType<typeof getFlats>>;
+  flats: Awaited<ReturnType<typeof getFlats>>;
 };
 
 export const loader = async () => {
-    return json<LoaderData>({
-        flats: await getFlats(),
-    });
+  return json<LoaderData>({
+    flats: await getFlats(),
+  });
 };
 
 export default function Flats() {
-    const { flats } = useLoaderData() as LoaderData;
+  const { flats } = useLoaderData() as LoaderData;
 
-    return (
-        <Stack>
-            {flats.map(flat => (
-                <React.Fragment key={flat.id}>
-                    <Typography variant="h4">{flat.title}</Typography>
-                    <Box borderRadius={1} bgcolor="primary">
-                        <img src={flat.image_url} alt={flat.title} />
-                    </Box>
-                </React.Fragment>
-            ))}
-        </Stack>
-    );
+  return (
+    <Stack>
+      {flats.map(flat => (
+        <FlatCard
+          key={flat.id}
+          title={flat.title}
+          imageUrl={flat.image_url}
+        />
+      ))}
+    </Stack>
+  );
 }

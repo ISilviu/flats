b479913824758c8b592679f4411db5b64bc39ec1
Ifrim Silviu <silviu.redent@gmail.com>
2022-07-03 13:39:06 +0300

    Format.

--- flats-app/app/routes/index.tsx
+++ flats-app/app/routes/index.tsx
@@ -36,7 +36,11 @@ export default function Flats() {
           </Grid>
         ))}
       </Grid>
-      <Pagination count={10} sx={{ py: 1 }} />
+      <Pagination
+        count={10}
+        sx={{ py: 1 }}
+        color="primary"
+      />
     </Stack>
   );
 }

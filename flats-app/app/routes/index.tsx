bfe88a835f6f7306607ec1f329922391ab7a5b52
Ifrim Silviu <silviu.redent@gmail.com>
2022-07-03 12:54:31 +0300

    Move flats to main page.

--- flats-app/app/routes/index.tsx
+++ flats-app/app/routes/index.tsx
@@ -1,10 +1,32 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Chip, Stack, Typography } from "@mui/material";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import React from "react";
+import { getFlats } from "~/models/flat.server";
 
-export default function IndexRoute() {
-  return (
-    <Box display="flex" gap={1}>
-      <Typography>Hi</Typography>
-      <Typography>There</Typography>
-    </Box>
-  );
+type LoaderData = {
+    flats: Awaited<ReturnType<typeof getFlats>>;
+};
+
+export const loader = async () => {
+    return json<LoaderData>({
+        flats: await getFlats(),
+    });
+};
+
+export default function Flats() {
+    const { flats } = useLoaderData() as LoaderData;
+
+    return (
+        <Stack>
+            {flats.map(flat => (
+                <React.Fragment key={flat.id}>
+                    <Typography variant="h4">{flat.title}</Typography>
+                    <Box borderRadius={1} bgcolor="primary">
+                        <img src={flat.image_url} alt={flat.title} />
+                    </Box>
+                </React.Fragment>
+            ))}
+        </Stack>
+    );
 }

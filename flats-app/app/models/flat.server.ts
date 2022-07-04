b56e178d0e6d1954e224b0aa3b62a7e9c047ab99
Ifrim Silviu <silviu.redent@gmail.com>
2022-07-04 20:48:22 +0300

    Add pagination support on the server.

--- flats-app/app/models/flat.server.ts
+++ flats-app/app/models/flat.server.ts
@@ -1,6 +1,22 @@
 import { PrismaClient } from "@prisma/client";
 
-export async function getFlats() {
-    const prisma = new PrismaClient();
-    return prisma.flat.findMany();
+type FlatsPaginationOptions = {
+    pageNumber: number;
+    pageSize: number;
+};
+
+const prisma = new PrismaClient();
+
+export async function getFlats({ pageSize, pageNumber }: FlatsPaginationOptions) {
+    return prisma.flat.findMany({
+        skip: pageSize * pageNumber,
+        take: pageSize,
+        orderBy: {
+            id: 'asc',
+        },
+    });
+}
+
+export async function getFlatsCount() {
+    return prisma.flat.count();
 }

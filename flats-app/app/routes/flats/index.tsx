84adc309d99c08ea9c35358bf438de060eec3c8a
Ifrim Silviu <silviu.redent@gmail.com>
2022-07-04 20:48:35 +0300

    Sync user interface.

--- flats-app/app/routes/flats/index.tsx
+++ flats-app/app/routes/flats/index.tsx
@@ -1,21 +1,53 @@
 import { Grid, Pagination, Stack } from "@mui/material";
 import FlatCard from "~/src/components/FlatCard";
 import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-import { getFlats } from "~/models/flat.server";
+import type { LoaderFunction } from "@remix-run/node";
+import { useLoaderData, useSearchParams } from "@remix-run/react";
+import { getFlats, getFlatsCount } from "~/models/flat.server";
+import { round } from "lodash";
+import React from "react";
 
 type LoaderData = {
     flats: Awaited<ReturnType<typeof getFlats>>;
+    flatsCount: Awaited<ReturnType<typeof getFlatsCount>>;
 };
 
-export const loader = async () => {
+const defaultPageSize = 50;
+
+const extractSearchParam = (searchParams: URLSearchParams, key: string, defaultValue: string) => {
+    let parameterValue = defaultValue;
+    if (searchParams.has(key)) {
+        parameterValue = searchParams.get(key) ?? defaultValue;
+        if (parameterValue === '') {
+            parameterValue = defaultValue;
+        }
+    }
+    return parameterValue
+};
+
+export const loader: LoaderFunction = async ({ request }) => {
+    const url = new URL(request.url);
+    const pageNumber = parseInt(extractSearchParam(url.searchParams, 'page', '0'));
+    const pageSize = parseInt(extractSearchParam(url.searchParams, 'pageSize', `${defaultPageSize}`));
+
     return json<LoaderData>({
-        flats: await getFlats(),
+        flats: await getFlats({
+            pageNumber: pageNumber,
+            pageSize: pageSize,
+        }),
+        flatsCount: await getFlatsCount(),
     });
 };
 
 export default function Flats() {
-    const { flats } = useLoaderData() as LoaderData;
+    const { flats, flatsCount } = useLoaderData() as LoaderData;
+
+    const [params, setSearchParams] = useSearchParams();
+
+    const pageNumber = parseInt(params.get('page') ?? '0') + 1;
+    const pageSize = parseInt(params.get('pageSize') ?? `${defaultPageSize}`);
+
+    const pagesCount = React.useMemo(() => round(flatsCount / pageSize), [flatsCount, pageSize]);
 
     return (
         <Stack
@@ -42,8 +74,9 @@ export default function Flats() {
                 ))}
             </Grid>
             <Pagination
-                count={10}
+                count={pagesCount}
                 sx={{ py: 1 }}
+                page={pageNumber}
                 color="primary"
             />
         </Stack>

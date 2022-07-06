354a0de71a18435728fefeba8f4c66e18c57def7
Ifrim Silviu <silviu.redent@gmail.com>
2022-07-06 21:58:14 +0300

    Checkpoint.

--- flats-app/crawler.js
+++ flats-app/crawler.js
@@ -1,8 +1,37 @@
-const browserObject = require('./browser');
-const scraperController = require('./pageController');
+const puppeteer = require('puppeteer');
 
-//Start the browser and create a browser instance
-let browserInstance = browserObject.startBrowser();
+const pageUrl = 'https://www.sreality.cz/en/search/for-sale/apartments';
 
-// Pass the browser instance to the scraper controller
-scraperController(browserInstance)
+async function startCrawling() {
+    const browser = await puppeteer.launch({
+        headless: false,
+        args: ["--disable-setuid-sandbox"],
+        'ignoreHTTPSErrors': true
+    });
+
+    const page = await browser.newPage();
+    await page.goto(pageUrl);
+
+    let texts = await page.evaluate(() => {
+        let apartments = [];
+
+        const apartmentTitles = document.getElementsByClassName('name ng-binding');
+        const apartmentDivElements = document.getElementsByClassName('property ng-scope');
+
+        let apartmentIndex = 0;
+        for (const apartmentElement of apartmentDivElements) {
+            apartments.push({
+                title: apartmentTitles[apartmentIndex].textContent,
+                image: apartmentElement.firstChild.firstChild.firstChild.firstChild.firstChild.getAttribute('src'),
+            });
+            ++apartmentIndex;
+        }
+
+        return apartments;
+    });
+
+    console.log(texts);
+    console.log(texts.length);
+}
+
+startCrawling();

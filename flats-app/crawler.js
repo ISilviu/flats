f651aeb6b666b6b72bed25d1bc47b8b958af2913
Ifrim Silviu <silviu.redent@gmail.com>
2022-07-07 21:10:42 +0300

    Add logging.

--- flats-app/crawler.js
+++ flats-app/crawler.js
@@ -1,4 +1,5 @@
 const { range } = require('lodash');
+const winston = require('winston');
 const { PrismaClient } = require('@prisma/client');
 const puppeteer = require('puppeteer');
 
@@ -6,9 +7,17 @@ const mainPageUrl = process.env.CRAWL_PAGE_URL;
 const urls = [mainPageUrl, ...range(2, 11).map(index => `${mainPageUrl}?page=${index}`)];
 const prismaClient = new PrismaClient();
 
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.File({ filename: process.env.LOG_PATH }),
+    ],
+});
+
 async function startCrawling() {
     const browser = await puppeteer.launch({
-        headless: false,
+        headless: true,
         args: ["--disable-setuid-sandbox"],
         'ignoreHTTPSErrors': true
     });
@@ -17,9 +26,18 @@ async function startCrawling() {
 
     const page = await browser.newPage();
 
-    console.log('Crawling ...');
+    logger.log({
+        level: 'info',
+        message: 'Started crawling ...',
+    });
+
     for (const url of urls) {
         await page.goto(url);
+        logger.log({
+            level: 'info',
+            message: `Navigated to: ${url}`,
+        });
+
         await page.waitForTimeout(1000);
         const currentPageFlats = await page.evaluate(() => {
             let flats = [];
@@ -27,7 +45,15 @@ async function startCrawling() {
             const flatTitles = document.getElementsByClassName('name ng-binding');
             const flatCards = document.getElementsByClassName('property ng-scope');
 
-            for(let flatIndex = 0; flatIndex < flatTitles.length; ++flatIndex) {
+            if (flatTitles.length != flatCards.length) {
+                logger.log({
+                    level: 'warn',
+                    message: `The flat titles count didn't match the number of flat cards found at url: ${url}\r\nThis page will be skipped to avoid errors.`,
+                });
+                return flats;
+            }
+
+            for (let flatIndex = 0; flatIndex < flatTitles.length; ++flatIndex) {
                 flats.push({
                     title: flatTitles[flatIndex].textContent,
                     image_url: flatCards[flatIndex].firstChild.firstChild.firstChild.firstChild.firstChild.getAttribute('src'),
@@ -40,14 +66,21 @@ async function startCrawling() {
         flats.push(...currentPageFlats);
     }
 
-    console.log('Inserting in the database ...');
-    await prismaClient.flat.createMany({
+    await browser.close();
+
+    logger.log({
+        level: 'info',
+        message: `Inserting ${flats.length} flats to the database ...`,
+    });
+
+    const { count: insertedCount } = await prismaClient.flat.createMany({
         data: flats,
     });
 
-    console.log('The apartments were inserted in the database.');
-
-    await browser.close();
+    logger.log({
+        level: 'info',
+        message: `${insertedCount} flats were successfully inserted to the database.`,
+    });
 }
 
 startCrawling();

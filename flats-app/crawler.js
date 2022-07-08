5d0e5a779a662ffec82f6bd023faf959df7897fe
Ifrim Silviu <silviu.redent@gmail.com>
2022-07-08 18:27:18 +0300

    Simplify logging

--- flats-app/crawler.js
+++ flats-app/crawler.js
@@ -25,17 +25,10 @@ async function startCrawling() {
 
     const page = await browser.newPage();
 
-    logger.log({
-        level: 'info',
-        message: 'Started crawling ...',
-    });
-
+    logger.info('Started crawling ...');
     for (const url of urls) {
         await page.goto(url);
-        logger.log({
-            level: 'info',
-            message: `Navigated to: ${url}`,
-        });
+        logger.info(`Navigated to: ${url}`);
 
         await page.waitForTimeout(1000);
         const currentPageFlats = await page.evaluate(() => {
@@ -45,10 +38,9 @@ async function startCrawling() {
             const flatCards = document.getElementsByClassName('property ng-scope');
 
             if (flatTitles.length != flatCards.length) {
-                logger.log({
-                    level: 'warn',
-                    message: `The flat titles count didn't match the number of flat cards found at url: ${url}\r\nThis page will be skipped to avoid errors.`,
-                });
+                logger.warn(
+                    `The flat titles count didn't match the number of flat cards found at url: ${url}\r\nThis page will be skipped to avoid errors.`
+                );
                 return flats;
             }
 
@@ -67,19 +59,13 @@ async function startCrawling() {
 
     await browser.close();
 
-    logger.log({
-        level: 'info',
-        message: `Inserting ${flats.length} flats to the database ...`,
-    });
+    logger.info(`Inserting ${flats.length} flats to the database ...`);
 
     const { count: insertedCount } = await prismaClient.flat.createMany({
         data: flats,
     });
 
-    logger.log({
-        level: 'info',
-        message: `${insertedCount} flats were successfully inserted to the database.`,
-    });
+    logger.info(`${insertedCount} flats were successfully inserted to the database.`);
 }
 
 startCrawling();

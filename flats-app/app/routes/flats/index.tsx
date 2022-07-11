0cb11b95b8efb8122b56221029c8974b5346c032
Ifrim Silviu <silviu.redent@gmail.com>
2022-07-11 08:14:48 +0300

    Move FlatsCard to new component

--- flats-app/app/routes/flats/index.tsx
+++ flats-app/app/routes/flats/index.tsx
@@ -1,12 +1,11 @@
-import { Grid, Pagination, Stack } from "@mui/material";
-import FlatCard from "~/src/components/FlatCard";
 import { json } from "@remix-run/node";
 import type { LoaderFunction } from "@remix-run/node";
-import { useLoaderData, useSearchParams } from "@remix-run/react";
+import { useLoaderData } from "@remix-run/react";
 import { getFlats, getFlatsCount } from "~/models/flat.server";
 import { round } from "lodash";
 import React from "react";
 import { extractSearchParam, parseIntWithDefault } from "~/src/utilities";
+import FlatsGrid from "~/src/components/FlatsGrid";
 
 type LoaderData = {
     flats: Awaited<ReturnType<typeof getFlats>>;
@@ -36,46 +35,16 @@ export const loader: LoaderFunction = async ({ request }) => {
 export default function Flats() {
     const { flats, flatsCount, pageNumber, pageSize } = useLoaderData() as LoaderData;
 
-    const [, setSearchParams] = useSearchParams();
-
     const pagesCount = React.useMemo(
         () => round(flatsCount / pageSize),
         [flatsCount, pageSize]
     );
 
     return (
-        <Stack
-            spacing={2}
-            pt={10}
-            maxHeight="100vh"
-            alignItems="center"
-        >
-            <Grid
-                container
-                rowGap={3}
-                columnGap={1.5}
-                display="flex"
-                justifyContent="center"
-                overflow="auto"
-            >
-                {flats.map(flat => (
-                    <Grid item xs={12} md={6} lg={3} key={flat.id} display="flex" justifyContent="center">
-                        <FlatCard
-                            title={flat.title}
-                            imageUrl={flat.image_url}
-                        />
-                    </Grid>
-                ))}
-            </Grid>
-            <Pagination
-                count={pagesCount}
-                sx={{ py: 1 }}
-                page={pageNumber}
-                color="primary"
-                onChange={(e, pageNumber) => setSearchParams({
-                    page: `${pageNumber}`,
-                })}
-            />
-        </Stack>
+        <FlatsGrid
+            flats={flats}
+            pageNumber={pageNumber}
+            pagesCount={pagesCount}
+        />
     );
 }
